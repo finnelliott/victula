9a5b493dc1ecbd0618a671bdfbd783a66f29bb71
finnelliott <finn@finnelliott.com>
2023-05-15 11:07:57 +0100

    --

--- src/components/application/dashboard/DashboardDiaryEntryDropdown.tsx
+++ src/components/application/dashboard/DashboardDiaryEntryDropdown.tsx
@@ -1,14 +1,16 @@
 "use client";
-import { Fragment } from 'react'
+import { Fragment, useState } from 'react'
 import { Menu, Transition } from '@headlessui/react'
 import { EllipsisVerticalIcon } from '@heroicons/react/20/solid'
 import Link from 'next/link';
+import DashboardEditFoodSlideover from './DashboardEditFoodSlideover';
 
 function classNames(...classes: string[]) {
     return classes.filter(Boolean).join(' ')
 }
 
 export default function DashboardDiaryEntryDropdown({ entry }: { entry: any }) {
+    const [editEntrySlideoverOpen, setEditEntrySlideoverOpen] = useState(false);
     async function deleteEntry(id: string) {
         const res = await fetch(`/api/entries/${id}`, {
             method: 'DELETE',
@@ -35,15 +37,15 @@ export default function DashboardDiaryEntryDropdown({ entry }: { entry: any }) {
             <Menu.Items className="absolute right-0 z-10 mt-2 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
             <Menu.Item>
                 {({ active }) => (
-                <Link
-                    href={`/app/entries/${entry.id}/edit`}
+                <button
+                    onClick={() => setEditEntrySlideoverOpen(true)}
                     className={classNames(
                     active ? 'bg-gray-50' : '',
                     'block px-3 py-1 text-sm leading-6 text-gray-900 w-full text-left'
                     )}
                 >
                     Edit<span className="sr-only">, {entry.name}</span>
-                </Link>
+                </button>
                 )}
             </Menu.Item>
             <Menu.Item>
@@ -61,6 +63,7 @@ export default function DashboardDiaryEntryDropdown({ entry }: { entry: any }) {
             </Menu.Item>
             </Menu.Items>
         </Transition>
+        <DashboardEditFoodSlideover entry={entry} open={editEntrySlideoverOpen} setOpen={setEditEntrySlideoverOpen} />
         </Menu>
     )
 }
